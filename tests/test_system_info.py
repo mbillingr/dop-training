cdea6f203aa3330d20b71f4c864524338bdabfb8
Martin Billinger <mbillingr@users.noreply.github.com>
2024-01-01 10:15:40 +0100

    add more unit tests

--- tests/test_system_info.py
+++ tests/test_system_info.py
@@ -1,5 +1,7 @@
+from datetime import datetime
+
 from dop_game import game, star_system
-from dop_game.core_types import SystemId
+from dop_game.core_types import SystemId, PlayerId
 
 
 def test_game_get_owning_player():
@@ -38,3 +40,39 @@ def test_system_list_ships():
     system_data = {"ships": {"fasty": 2, "slowy": 1}}
     ships = star_system.list_ships(system_data)
     assert ships == {"fasty": 2, "slowy": 1}
+
+
+def test_game_production_status_same_player():
+    game_data = {
+        "star_systems_by_id": {
+            "alpha": {
+                "owning_player": "hotzenplotz",
+                "building_slot": {"eta": datetime(6042, 1, 1), "kind": "civilian"},
+            }
+        }
+    }
+    slot = game.get_production_status(
+        game_data, PlayerId("hotzenplotz"), SystemId("alpha")
+    )
+    assert slot == {"eta": datetime(6042, 1, 1), "kind": "civilian"}
+
+
+def test_game_production_status_other_player():
+    game_data = {
+        "star_systems_by_id": {
+            "alpha": {
+                "owning_player": "hotzenplotz",
+                "building_slot": {"eta": datetime(6042, 1, 1), "kind": "civilian"},
+            }
+        }
+    }
+    slot = game.get_production_status(
+        game_data, PlayerId("rübezahl"), SystemId("alpha")
+    )
+    assert slot is None
+
+
+def test_system_production_status():
+    system_data = {"building_slot": {"eta": datetime(6042, 1, 1), "kind": "civilian"}}
+    slot = star_system.get_production_status(system_data)
+    assert slot == {"eta": datetime(6042, 1, 1), "kind": "civilian"}
